97e82bfda91f61c86ed2aa10b3d2281254ce16a5
DevBrendaReis <dev.brendareis@gmail.com>
2022-10-09 18:26:13 -0300

    first commit

---
+++ soma.py
@@ -0,0 +1,10 @@
+ 
+numero1 = input("Digite o primeiro numero: ")
+numero2 = input("Digite o segundo numero: ")
+soma = int(numero1) +  int(numero2)
+    
+print()
+print(f'A soma de {numero1} + {numero2} é igual a: {soma}')
+    
+
+    
